b7eff03cb50814e0f220c9891a26a3b485552a67
xilix <albert.rosell@gmail.com>
2016-04-02 17:47:44 +0200

    module to hold the state of the app

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+var State = {
+  filter: {
+    textSearch: '',
+    begin: new Date().getFullYear(),
+    end: new Date().getFullYear(),
+    timeSpan: 'm'
+  },
+  trends: []
+};
+
--- test/main.js
+++ test/main.js
@@ -1,7 +1,31 @@
 'use strict';
 
-describe('config test', function () {
-  it('run tests with karma', function () {
-    expect(true).toBe(false);
+describe('Main', function () {
+  describe('State objects', function () {
+    describe('Filter contains user selected filters', function () {
+      it('have to be defined', function () {
+        expect(State.filter !== undefined).toBe(true);
+      });
+      it('textSearch filter should be empty at the beginning', function () {
+        expect(State.filter.textSearch).toBe('');
+      });
+      it('begin and end should be the time bounds', function () {
+        expect(State.filter.begin <= State.filter.end).toBe(true);
+      });
+      it('time stamp should be a time unit', function () {
+        expect(
+          ['y', 'm', 'd'].indexOf(State.filter.timeSpan) !== -1
+        ).toBe(true);
+      });
+    });
+    describe('Trends contains the output trends data', function () {
+      it('should be defined at the beginning', function () {
+
+        expect(State.trends !== undefined).toBe(true);
+      });
+      it('should have empty items at the beginning', function () {
+        expect(State.trends.length).toBe(0);
+      });
+    });
   });
 });
