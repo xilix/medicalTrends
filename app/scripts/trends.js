8b18d9514865f7ff585c410cc0031d93bcdecca0
xilix <albert.rosell@gmail.com>
2016-04-03 23:11:54 +0200

    trends list

--- app/scripts/trends.js
+++ app/scripts/trends.js
@@ -1,24 +1,50 @@
 var Trends = (function () {
   return {
+    destroyTrend: function (trend) {
+      State.trends[trend] = undefined;
+      delete State.trends[trend];
+    },
     meldWithTrends: function (trend, data) {
       var trendDates;
 
-      if (State.trends[trend] === undefined) {
-        State.trends[trend] = data;
+      if (trend) {
+        if (State.trends[trend] === undefined) {
+          State.trends[trend] = data;
+        } else {
+          // do not microoptimize when is not really need
+          // and there is a certain drawback in maintanability
+          trendDates = State.trends[trend].map(function (tr) {
+            return tr.year;
+          });
+          data.forEach(function (item) {
+            if (trendDates.indexOf(item.year) === -1) {
+              State.trends[trend].push(item);
+            }
+          });
+          State.trends[trend].sort(function (item1, item2) {
+            return item1.year - item2.year;
+          });
+        }
+      }
+    },
+    writeTrendListInElement: function (id) {
+      var element = document.getElementById(id);
+
+      if (Object.keys(State.trends).length) {
+        element.innerHTML = [
+          '<ul class="trends-list">',
+            Object.keys(State.trends).map(function (trend) {
+              return [
+                '<li>',
+                  '<a class="trend-item" href="#" rel="', trend, '">', trend, '</a>',
+                  '<a class="trend-destroy" href="#" onclick="Trends.destroyTrend(\'', trend, '\');Trends.writeTrendListInElement(\'', id, '\');">x</a>',
+                '</li>'
+              ].join('');
+            }).join(''),
+          '</ul>'
+        ].join('');
       } else {
-        // do not microoptimize when is not really need
-        // and there is a certain drawback in maintanability
-        trendDates = State.trends[trend].map(function (tr) {
-          return tr.year;
-        });
-        data.forEach(function (item) {
-          if (trendDates.indexOf(item.year) === -1) {
-            State.trends[trend].push(item);
-          }
-        });
-        State.trends[trend].sort(function (item1, item2) {
-          return item1.year - item2.year;
-        });
+        element.innerHTML = '';
       }
     }
   };
